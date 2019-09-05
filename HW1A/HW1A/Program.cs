b210638459f1c4b4c79c0033057b1b19ef0f2170
Khanh Le <lek5763@uhcl.edu>
2019-09-05 12:49:47 -0500

    HW1A

---
+++ HW1A/HW1A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number 1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter number 2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter an operator: (+, -, *, /)");
+            string op = Console.ReadLine();
+
+            if (op == "+")
+            {
+                int sum = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {sum}");
+            }
+            if (op == "-")
+            {
+                int diff = num1 - num2;
+                Console.WriteLine($"{num1} - {num2} = {diff}");
+            }
+            if (op == "*")
+            {
+                int pro = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = {pro}");
+            }
+            if (op == "/")
+            {
+                int div = num1 / num2;
+                Console.WriteLine($"{num1} / {num2} = {div}");
+            }
+
+        }
+    }
+}
